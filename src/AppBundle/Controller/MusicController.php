2a58ffe892ef829ea32b7107741d55f69b43ff6a
Gwalchmei <darainas2@gmail.com>
2016-02-06 14:16:21 +0100

    create music controller

---
+++ src/AppBundle/Controller/MusicController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gauvain
+ * Date: 06/02/16
+ * Time: 14:08
+ */
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+
+/**
+ * @Route("/music")
+ * Class MusicController
+ * @package AppBundle\Controller
+ */
+class MusicController extends Controller
+{
+
+}
