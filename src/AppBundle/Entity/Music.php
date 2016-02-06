6f11aa5990a6d3c42f9982f862a3bf8dac11e127
Gwalchmei <darainas2@gmail.com>
2016-02-06 14:16:02 +0100

    create music entity

---
+++ src/AppBundle/Entity/Music.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gauvain
+ * Date: 06/02/16
+ * Time: 12:45
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Class Music
+ * @ORM\Table()
+ * @ORM\Entity()
+ * @package AppBundle\Entity
+ */
+class Music
+{
+    /**
+     * @var integer
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $name;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     * @Assert\File(mimeTypes={'audio/mpeg'}
+     */
+    protected $path;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param $name
+     * @return $this
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param $file
+     * @return $this
+     */
+    public function setPath($path)
+    {
+        $this->path= $path;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+}
