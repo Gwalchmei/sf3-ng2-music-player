500ae53fa42aa394ce60ee6ce7eb8194085c5b16
Gwalchmei <darainas2@gmail.com>
2016-02-06 14:59:14 +0100

    add streamAction and getAction with db fetch

--- src/AppBundle/Controller/MusicController.php
+++ src/AppBundle/Controller/MusicController.php
@@ -8,6 +8,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Music;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -21,5 +23,48 @@ use Symfony\Component\HttpFoundation\StreamedResponse;
  */
 class MusicController extends Controller
 {
+    /**
+     * @Route("/stream/{id}", name="stream_music", requirements={"id" = "[0-9]+"})
+     * @param integer $id
+     * @return StreamedResponse
+     */
+    public function streamAction($id)
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER', null, 'Unable to access this page!');
+        $music = $this->getDoctrine()->getManager()->getRepository("AppBundle:Music")->find($id);
+        if (!$music instanceof Music) {
+            throw $this->createNotFoundException('No music found for id '.$id);
+        }
+        $file = 'music/'.$music->getPath();
 
+        return new StreamedResponse(
+            function () use ($file) {
+                readfile($file);
+            }, 200, array('Content-Type' => 'audio/mpeg')
+        );
+    }
+
+    /**
+     * @Route("/{id}", name="get_music", requirements={"id" = "[0-9]+"})
+     * @Method("GET")
+     * @param $id
+     * @return JsonResponse
+     */
+    public function getAction($id)
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER', null, 'Unable to access this page!');
+        $music = $this->getDoctrine()->getManager()->getRepository("AppBundle:Music")->find($id);
+        if (!$music instanceof Music) {
+            throw $this->createNotFoundException('No music found for id '.$id);
+        }
+
+        $response = new JsonResponse();
+        $response->setData(
+            array(
+                'data' => array("id" => $music->getId(), "name" => $music->getName())
+            )
+        );
+
+        return $response;
+    }
 }
