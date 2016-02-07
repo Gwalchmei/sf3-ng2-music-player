f292f56eabcc0a5ee5b26114870bcc0d1acd6537
Gwalchmei <darainas2@gmail.com>
2016-02-07 01:31:35 +0100

    loop over getElementsByTagName('link') to avoid error message in ts compilation

--- web/app/base-url.ts
+++ web/app/base-url.ts
@@ -1,1 +1,12 @@
-export var BASEURL = document.querySelector("link[rel='canonical']").href;
+export var BASEURL = findCanonical();
+
+function findCanonical()
+{
+    var links = document.getElementsByTagName('link');
+    for (var i=0; i<links.length;i++) {
+        var link = links[i];
+        if (link.rel == 'canonical') {
+            return link.href;
+        }
+    }
+}
