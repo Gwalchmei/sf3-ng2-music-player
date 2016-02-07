d29a33da110f37afdd55d7ca383489842924813f
Gwalchmei <darainas2@gmail.com>
2016-02-07 01:33:51 +0100

    use Observable/Observer to provide musics

--- web/app/music.service.ts
+++ web/app/music.service.ts
@@ -2,20 +2,35 @@ import {Injectable} from 'angular2/core';
 import {Http}       from 'angular2/http';
 import {Music}      from './music.js';
 import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/share';
 import {BASEURL} from './base-url.js';
 
 @Injectable()
 export class MusicService {
-    constructor (private _http: Http) {}
-    private _listUrl = BASEURL+'list/';
-    getMusics () {
-        return this._http.get(this._listUrl)
-                    .map(res => <Music[]> res.json().data)
-                    .catch(this.logAndPassOn);
+    musics$: Observable<Array<Music>>;
+    private _musicsObserver: any;
+    private _dataStore: {
+        musics: Array<Music>
+    };
+
+    constructor (private _http: Http) {
+        this.musics$ = new Observable(observer =>
+            this._musicsObserver = observer).share();
+
+        this._dataStore = { musics: [] };
     }
 
-    private logAndPassOn (error: Error) {
+    loadMusics(id?: number) {
+        var url = BASEURL+"music/";
+        if (id !== undefined) {
+            url = url+"?pid="+id;
+        }
+        this._http.get(url).map(response => <Music[]> response.json().data).subscribe(musics => {
+            // Update data store
+            this._dataStore.musics = musics;
 
-        return Observable.throw(error);
+            // Push the new list of musics into the Observable stream
+            this._musicsObserver.next(this._dataStore.musics);
+        }, error => console.log('Could not load musics.'));
     }
 }
